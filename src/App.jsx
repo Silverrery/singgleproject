14a83766fa37e8a1cee80a814d1d7e317846b7d7
Silverrery <122677350+Silverrery@users.noreply.github.com>
2024-02-11 19:02:39 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import LoginForm from "./layout/LoginForm";
+
+function App() {
+
+  return (
+    <div className="min-h-screen">
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layout/RegisterForm.jsx
@@ -0,0 +1,78 @@
+import axios from 'axios'
+import {useState} from "react";
+
+export default function RegisterForm() {
+  const [input, setInput] = useState({
+    email : '', 
+    password : '',
+    full_name : ''
+    
+  })
+
+  const hdlChange = e => {
+    setInput( prv => ( { ...prv, [e.target.name] : e.target.value } ) )
+  }
+
+  const hdlSubmit = async e => {
+    try {
+      e.preventDefault()
+      const rs = await axios.post('http://localhost:8001/auth/register', input)
+      console.log(rs)
+      if(rs.status === 200) {
+        alert('Register Successful')
+      }
+    }catch(err) {
+      console.log( err.message)
+    }
+
+  }
+
+  return (
+    <div className="p-5 border w-4/6 min-w-[500px] mx-auto rounded mt-5 ">
+      <div className="text-3xl mb-5">Register Form</div>
+      <form className="flex flex-col gap-2" onSubmit={hdlSubmit}>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">E-mail</span>
+          </div>
+          <input
+            type="email"
+            className="input input-bordered w-full max-w-xs"
+            name="email"
+            value={input.email}
+            onChange={ hdlChange }
+          />
+        </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">password</span>
+          </div>
+          <input
+            type="password"
+            className="input input-bordered w-full max-w-xs"
+            name="password"
+            value={input.password}
+            onChange={ hdlChange }
+          />
+        </label>
+        <label className="form-control w-full max-w-xs">
+          <div className="label">
+            <span className="label-text">full_name</span>
+          </div>
+          <input
+            type="text"
+            className="input input-bordered w-full max-w-xs"
+            name="full_name"
+            value={ input.full_name }
+            onChange={ hdlChange }
+          />
+        </label>
+       
+        <div className="flex gap-5 ">
+          <button type="submit" className="btn btn-outline btn-info mt-7">Submit</button>
+          <button type="reset" className="btn btn-outline btn-warning mt-7">Reset</button>
+        </div>
+      </form>
+    </div>
+  );
+}
